fbd08e5a93b570087a602f737562e22e9cf8fb87
ydjin:3 <e.mishchenko@cyrm.ru>
2026-02-03 06:24:23 +0700

    Ойй

--- app/api/utils/swagger/ui.py
+++ app/api/utils/swagger/ui.py
@@ -10,7 +10,7 @@ from starlette.responses import HTMLResponse
 def get_swagger_ui_html(
     *,
     openapi_url: str = '/openapi.json',
-    title: str = 'VPN-PORT',
+    title: str = 'Template Service',
     swagger_js_url: str = 'https://cdn.jsdelivr.net/npm/swagger-ui-dist@5.9.0/swagger-ui-bundle.js',
     swagger_css_url: str = 'https://cdn.jsdelivr.net/npm/swagger-ui-dist@5.9.0/swagger-ui.css',
     swagger_favicon_url: str = 'https://fastapi.tiangolo.com/img/favicon.png',
